fd91f114800a61cc5ca62ed943517be345dd380a
shiro24neko <133599652+shiro24neko@users.noreply.github.com>
2023-06-18 23:25:05 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context/productcontex";
+import { FilterContextProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const domain = process.env.REACT_APP_AUTH_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+root.render(
+  <Auth0Provider
+    domain="dev-2aphyjhxap8y1qyc.us.auth0.com"
+    clientId="iNpsSdeRBsCDMU6ao6OSHKve87cEwicv"
+    redirectUri={window.location.origin}>
+    <AppProvider>
+      <FilterContextProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterContextProvider>
+    </AppProvider>
+  </Auth0Provider>
+);
+
+reportWebVitals();
